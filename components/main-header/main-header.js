fcfba00e6d444cdb635092d1b5792108fbf96a86
GerrinM <gerrin.murray@gmail.com>
2024-06-11 11:05:03 -0500

    outputting meals data & images with unknown dimensions

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,13 +2,13 @@ import Link from "next/link";
 import logoImage from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from "next/image";
-// import MainHeaderBackground from "./main-header-background";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
     console.log('Executing MainHeader...')
     return (
         <>
-        {/* <MainHeaderBackground /> */}
+        <MainHeaderBackground />
         <header className={classes.header}>
             
                 <Link className={classes.logo} href="/">
