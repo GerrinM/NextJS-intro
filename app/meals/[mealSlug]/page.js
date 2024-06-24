794727bc32bacfb5a69d5e987d3e957f25829e2a
GerrinM <gerrin.murray@gmail.com>
2024-06-24 09:47:32 -0500

    error handling

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,11 @@
 export default function MealsDetailsPage () {
-    return (
+    return (<>
+        <header>
+
+        </header>
         <main>
             <h1>Meal Details</h1>
             <p>Details for </p>
         </main>
-    )
+        </>)
 }
