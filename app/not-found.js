6e84512fea72a59f3a931840d83d82ac17737331
GerrinM <gerrin.murray@gmail.com>
2024-06-24 09:51:52 -0500

    handling not found

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Not Found!</h1>
+        <p>Sorry, the page you are looking for cannot be found.</p>
+    </main>
+}
