ba58fff24175cfb4effa00604ad44f63ac48e518
GerrinM <gerrin.murray@gmail.com>
2024-06-24 10:13:01 -0500

    meal not found

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return <main className="not-found">
         <h1>Not Found!</h1>
-        <p>Sorry, the page you are looking for cannot be found.</p>
+        <p>Sorry, the meal you are looking for cannot be found.</p>
     </main>
 }
